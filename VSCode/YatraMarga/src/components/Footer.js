ed510c9101f1cf1e127396b24d2f3f6c3bbd04cc
Cb18_spirit <18chethan2006@gmail.com>
2025-02-04 15:10:33 +0530

    yatramarga2

---
+++ VSCode/YatraMarga/src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 YatraMarga. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ VSCode/YatraMarga/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h2>Welcome to YatraMarga!</h2>
+      <p>Explore the best travel destinations.</p>
+    </div>
+  );
+}
+
+export default HomePage;
