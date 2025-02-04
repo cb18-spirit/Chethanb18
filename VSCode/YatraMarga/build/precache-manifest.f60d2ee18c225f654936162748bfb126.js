28c532e2aa81b239abe2bc70223ddc1295a4a5cf
Cb18_spirit <18chethan2006@gmail.com>
2025-02-04 14:50:19 +0530

    Yatramarga1

---
+++ VSCode/YatraMarga/build/precache-manifest.f60d2ee18c225f654936162748bfb126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764d9427989b95d618a57d1b528c4d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "085cf04978fee1e78cd6",
+    "url": "/static/css/main.fd14eeac.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "085cf04978fee1e78cd6",
+    "url": "/static/js/main.1312e7ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
