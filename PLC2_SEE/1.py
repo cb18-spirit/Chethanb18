b0cc694b375d0eee0a059971d965314526b5a9e3
Cb18_spirit <18chethan2006@gmail.com>
2025-03-07 10:54:54 +0530

    Anvaya_Restart

--- PLC2_SEE/1.py
+++
@@ -1,7 +0,0 @@
-import shelve 
-with shelve.open("data_store") as db: 
-    db["name"] = "Chethan" 
-    db["age"] = 18  
-with shelve.open("data_store") as db: 
-    print(db["name"])   
-    print(db["age"])  
--- PLC2_SEE/3.py
+++
@@ -1,6 +0,0 @@
-with open(r"C:\Users\18che\Code_Projects\Chethanb18\PLC2_SEE\sample.txt", "r") as file: 
-    content = file.read()  # Reads entire file as a string 
-    lines = file.readlines()  # Reads file into a list of lines
-
-print(content)
-print(lines)
